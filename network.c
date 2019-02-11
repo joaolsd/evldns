d63fc8013f1445f92dd4bd0ad1b1140a2f7d726f
João Damas <2915615+joaolsd@users.noreply.github.com>
2019-02-11 15:48:33 +0100

    Revert "Changes to support binding to more than one address (several -h options)" Got tired of having apnic-master, apnic-glueless and evldns always out of sync with the way of doing multiple -h options I have decided to keep my way, even if Ray's is more elegant, because I am tired of C++ and just want to get things working consistently instead of spending time  editing the same shit each time This reverts commit 1c9745288c4350d37ca8096f2d30b47316d1e685.

--- network.c
+++ network.c
@@ -186,19 +186,21 @@ int bind_to_tcp6_port(int port, int backlog)
 
 /*--------------------------------------------------------------------*/
 
+<<<<<<< HEAD
 int *bind_to_all(const char *hostname, const char *port, int backlog)
+=======
+int *bind_to_all(const char *ipaddr, const char *port, int backlog)
+>>>>>>> parent of 1c97452... Changes to support binding to more than one address (several -h options)
 {
 	struct sockaddr_storage	addr;
 	struct addrinfo			hints, *ai, *ai0;
 	int						*result = 0;
-  const char *ipaddr;
-  int count_all = 0;
-  int i = 0;
-  
+
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = PF_UNSPEC;
 	hints.ai_flags = AI_ADDRCONFIG | AI_PASSIVE;
 
+<<<<<<< HEAD
   int current = 0;
 
     ipaddr = hostname;
@@ -248,6 +250,38 @@ int *bind_to_all(const char *hostname, const char *port, int backlog)
         result[current++] = fd;
       }
     }
+=======
+	int res = getaddrinfo(ipaddr, port, &hints, &ai);
+	if (res) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(res));
+		return NULL;
+	}
+	ai0 = ai;
+
+	/* count the addrinfo objects */
+	int count = 0;
+	while (ai) {
+		++count;
+		ai = ai->ai_next;
+	}
+
+	/* make some memory for FDs */
+	result = (int *)calloc(count + 1, sizeof(int));
+
+	int current = 0;
+	for (ai = ai0 ; ai; ai = ai->ai_next) {
+		if (ai->ai_socktype != SOCK_DGRAM && ai->ai_socktype != SOCK_STREAM) continue;
+
+		int addrlen = ai->ai_addrlen;
+		memset(&addr, 0, sizeof(addr));
+		memcpy(&addr, ai->ai_addr, addrlen);
+
+		int fd = bind_to_sockaddr((struct sockaddr *)&addr, addrlen, ai->ai_socktype, backlog);
+		if (fd >= 0) {
+			result[current++] = fd;
+		}
+	}
+>>>>>>> parent of 1c97452... Changes to support binding to more than one address (several -h options)
 
 	/* clean up and terminate */
 	freeaddrinfo(ai0);
